5693f73d8fc96828a60b2ecf23a0b013f5725552
Miguel Barraza <miguelbarraza2015@gmail.com>
2018-06-01 12:07:41 -0300

    incorporado run, el ejecutador de peticiones de la api

--- server.js
+++ server.js
@@ -2,50 +2,61 @@ let express = require('express');
 let app = express();
 const fs = require('fs');
 const unqmod = require('./unqfy');
+//const {isNotUndefined} = require('./funcionesAuxiliares');
 
 let router = express.Router();
 
-let port = process.env.PORT || 8080;        // set our port
+let port = process.env.PORT || 8080;
 
-// Retorna una instancia de UNQfy. Si existe filename, recupera la instancia desde el archivo.
 function getUNQfy(filename) {
   let unqfy = new unqmod.UNQfy();
   if (fs.existsSync(filename)) {
-    console.log();
     unqfy = unqmod.UNQfy.load(filename);
   }
   return unqfy;
 }
 
-// Guarda el estado de UNQfy en filename
 function saveUNQfy(unqfy, filename) {
-  console.log();
   unqfy.save(filename);
 }
 
 
 router.use(function (req, res, next) {
     console.log('Request received!');
-    next(); // make sure we go to the next routes and don't stop here
-});
+    next();});
 
 router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.use('/api', router);
-router.route('/artist').get(function (req, res) {
-    if (req.query.name && req.query.country){
-        let unqfy = getUNQfy('estado.json');
-unqfy.addArtist(req.query);
-        saveUNQfy(unqfy, 'estado.json');
-        res.json({ message: 'okey' });
-    }else{
-        res.json({ message: 'Se esperaba: name, country ' });
+function isNotUndefined(value) {
+  return value != undefined;
 }
-});
 
 
+function run(params, func) {
+return function (req, res) {
+  if(params.every(p => isNotUndefined(req.query[p]))) {
+        let unqfy = getUNQfy('estado.json');
+r=func(unqfy, req.query);
+        saveUNQfy(unqfy, 'estado.json');
+        res.json(r);
+    }else{
+        res.json({ status: 400, errorCode: 'BAD_REQUEST'});
+} };
+}
+
+router.route('/artist').get(run(['id'], function (unqfy, data) {
+//unqfy.addArtist(data);
+return { message: 'dok' };
+}));
+
+router.route('/artist').post(run(['name','country'], function (unqfy, data) {
+unqfy.addArtist(data);
+return { message: 'oky' };
+}));
+
+app.use('/api', router);
 
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Server started at the port: ' + port);
